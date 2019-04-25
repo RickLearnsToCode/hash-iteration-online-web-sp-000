a75689c32ab46ec47e6404c3c479132a50a2ffe0
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-25 16:56:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,7 @@ def age_appropriate_birthday(hash)
   hash.each do |kid, age|
     if age < 13
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    else puts "You are too old for this."
     end
   end
 end
