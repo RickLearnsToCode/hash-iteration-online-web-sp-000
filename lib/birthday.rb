f2f0bd778f77c08d6eceb02c56e379e60ded43d3
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-25 16:55:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,14 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
+
+
+def age_appropriate_birthday(hash)
+  hash.each do |kid, age|
+    if age < 12
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    end
+  end
+end
+    
+    
